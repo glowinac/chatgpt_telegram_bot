aeb37b23f93af8be7485e311e119f8e1220103aa
glowinac <glowinac@gmail.com>
2023-05-12 12:06:31 +0800

    fix: Specify linux/amd64 as platform for Python installation

--- bot/config.py
+++ bot/config.py
@@ -21,6 +21,7 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
 mongodb_uri = config_yaml["mongo_connect_string"]
+# mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
